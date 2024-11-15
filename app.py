3266b0e1f271e06e916714b994f8fd24094a52c0
saifbagmaru <163076009+saifbagmaru@users.noreply.github.com>
2024-11-16 04:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import pandas as pd
 from mistralai_azure import MistralAzure
 import re
-from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import logging
